541aa7d0b3d12a88470b677631b8289e061422f7
Magalie <magalie.kniola@gmail.com>
2025-05-07 09:11:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li><a href="./affichages">Affichages</a></li>
+        <li><a href="./affichages/concatenation.php">Affichages-concatenation</a></li>
+        <li><a href="./variables">Variables</a></li>
+        <li><a href="./variables/types.php">Variables-types</a></li>
+        <li><a href="./variables/references.php">Variables-references</a></li>
+        <li><a href="./variables/constantes.php">Variables-constantes</a></li>
+        <li><a href="./conditions">Conditions-opérateurs</a></li>
+        <li><a href="./fonctions/predefinies.php">Fonctions-prédefinies</a></li>
+        <li><a href="./fonctions/utilisateur.php">Fonctions-utilisateur</a></li>
+        <li><a href="./boucles/boucles.php">boucles</a></li>
+        <li><a href="./boucles/for.php">boucles-for</a></li>
+        <li><a href="./array/bases.php">Array-bases</a></li>
+    </ul>
+</nav>
